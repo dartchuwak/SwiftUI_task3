7a67fe3443cc66dfc796bb030b4a0a63d9ef6756
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-07 17:49:10 +0300

    fix

--- SwiftUI_task3/ContentView.swift
+++ SwiftUI_task3/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var offset = 0.0
-    @State var scale = CGSize(width: 0, height: 0)
-    @State var scale2 = CGSize(width: 1, height: 1)
+    @State var scale = 0.0
+    @State var scale2 = 1.0
     @State var opacity1 = 0.0
     @State var opacity2 = 1.0
     let imageSize = 24.0
@@ -22,10 +22,8 @@ struct ContentView: View {
         Button(action: {
             withAnimation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) {
                 self.offset = imageSize + spacing
-                self.scale = CGSize(width: 1, height: 1)
-            }
-            withAnimation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) {
-                self.scale2 = CGSize(width: 0, height: 0)
+                self.scale = 1.0
+                self.scale2 = 0.0
                 self.opacity2 = 0
                 self.opacity1 = 1
             }
@@ -33,8 +31,8 @@ struct ContentView: View {
 
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 self.offset = 0
-                self.scale = CGSize(width: 0, height: 0)
-                self.scale2 = CGSize(width: 1, height: 1)
+                self.scale = 0.0
+                self.scale2 = 1.0
                 self.opacity2 = 1
                 self.opacity1 = 0
                 self.anchor = .leading
@@ -46,24 +44,18 @@ struct ContentView: View {
                     .resizable()
                     .renderingMode(.template)
                     .foregroundStyle(Color.blue)
-                    .frame(width: imageSize, height: imageSize)
-                    .offset(x: self.offset)
-                    .scaleEffect(CGSize(width: 1.0, height: 1.0))
-                    .overlay {
-                        Image("play")
-                            .resizable()
-                            .renderingMode(.template)
-                            .foregroundStyle(Color.blue)
-                            .frame(width: imageSize, height: imageSize)
-                            .scaleEffect(scale, anchor: anchor)
-                            .opacity(opacity1)
-                    }
+                    .frame(width: imageSize * scale, height: imageSize * scale)
+                    .opacity(opacity1)
                 Image("play")
                     .resizable()
                     .renderingMode(.template)
                     .foregroundStyle(Color.blue)
                     .frame(width: imageSize, height: imageSize)
-                    .scaleEffect(scale2, anchor: .trailing)
+                Image("play")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundStyle(Color.blue)
+                    .frame(width: imageSize * scale2, height: imageSize * scale2)
                     .opacity(opacity2)
             }
         }
